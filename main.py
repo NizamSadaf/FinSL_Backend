037f4c3072341902236d2001b68d45095b1f459c
unknown <nizamsadaf1460@gmail.com>
2026-04-28 13:54:41 +0300

    CORS updated

--- main.py
+++ main.py
@@ -383,6 +383,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://fin-sl-frontend.vercel.app/",
     ],
     allow_credentials=True,
     allow_methods=["*"],
